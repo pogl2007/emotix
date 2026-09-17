92101a18aa52f5e0dd519764878d73450adf6030
pogl2007 <vladkosolapovpogba@gmail.com>
2026-09-17 17:05:12 +0300

    добавил докер

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Самодостаточная сборка для Docker: server.js + только нужные node_modules
+  output: "standalone",
 };
 
 module.exports = nextConfig;
